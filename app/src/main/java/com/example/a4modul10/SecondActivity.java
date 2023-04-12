fe3c8eafe3e4a5d9715aefd750e384b3789fa4ae
angelina <ashapovalova2007@gmail.com>
2023-04-12 22:13:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a4modul10/SecondActivity.java
@@ -0,0 +1,72 @@
+package com.example.a4modul10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondActivity  extends AppCompatActivity implements SecondActivity3 {
+
+    TextView nameTextView = findViewById(R.id.editTextTextPersonName);
+    TextView departureTextView = findViewById(R.id.editTextTextPersonName2);
+    TextView arrivalTextView = findViewById(R.id.editTextTime2);
+    TextView departureTimeTextView = findViewById(R.id.editTextTime3);
+    TextView arrivalTimeTextView = findViewById(R.id.editTextNumber);
+    TextView priceTextView = findViewById(R.id.editTextNumber2);
+    Button resetButton = findViewById(R.id.button2);
+
+    // Получение данных из интента и установка их в TextView элементы
+    Bundle extras = getIntent().getExtras();
+     if(extras !=null)
+
+    {
+
+
+        {
+            nameTextView.setText(extras.getString("name"));
+            departureTextView.setText(extras.getString("departure"));
+            arrivalTextView.setText(extras.getString("arrival"));
+            departureTimeTextView.setText(extras.getString("departureTime"));
+            arrivalTimeTextView.setText(extras.getString("arrivalTime"));
+            priceTextView.setText(extras.getString("price"));
+        }
+
+// Обработчик нажатия на кнопку resetButton
+        resetButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                // Создание экземпляра класса Intent для перехода на MainActivity
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+
+                // Запуск MainActivity с очисткой данных
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+
+    ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/a4modul10/SecondActivity3.java
@@ -0,0 +1,7 @@
+package com.example.a4modul10;
+
+import android.view.View;
+
+public interface SecondActivity3 {
+    void onClick (View v);
+}
